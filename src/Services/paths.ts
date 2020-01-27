4d2432c130661b5b75e1d593eb5bb707b9f46e89
Cannon <kcanno045@cable.comcast.com>
2020-01-26 21:46:44 -0500

    Added env security and fixed errors

--- src/Services/paths.ts
+++ src/Services/paths.ts
@@ -1,4 +1,6 @@
-export const baseUrl: string = 'https://api.giphy.com/v1/gifs/search?api_key=qYpiHaLorSOiBFat1wXyu7R33nKeweQr';
+const API_KEY = process.env.REACT_APP_API_KEY
+
+export const baseUrl: string = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}`;
 
 export const queryParams: string = '&limit=25&offset=0&rating=G&lang=en';
 
--- src/Store/middleware.ts
+++ src/Store/middleware.ts
@@ -3,13 +3,23 @@ import {SearchDataActions, AppDispatch} from '../Types/types';
 import {GET_SEARCH_DATA, LOADING, SEARCH_DATA_SUCCESS, SEARCH_DATA_FAILURE} from '../Store/actions';
 
 export const applyMiddleware = (action: SearchDataActions, dispatch: AppDispatch) => {
+    function handleErrors(response: any) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }
+
 switch(action.type) {
     case GET_SEARCH_DATA:
         dispatch({type: LOADING, payload: ''});
         fetch(`${baseUrl}&q=${action.payload}${queryParams}`)
-        .then((response) => {return response.json();})
+        .then(handleErrors)
+        .then((response) => response.json())
         .then(data => dispatch({type: SEARCH_DATA_SUCCESS, payload: data.data}))
-        .catch(error => dispatch({type: SEARCH_DATA_FAILURE, payload: ''}));
+        .catch(error => {
+            dispatch({type: SEARCH_DATA_FAILURE, payload: ''})
+        });
         break;
     };
 }
